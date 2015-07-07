0715cba240af97ae742232bef20ee00f0d6cd4a4
Dmitry Orlov <me@mosquito.su>
2015-07-07 12:40:43 +0300

    [fea] add sertup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from __future__ import absolute_import, print_function
+import os
+
+from setuptools import setup, find_packages
+
+
+__version__ = '0.1alpha'
+__author__ = 'Dmitry Orlov <me@mosquito.su>'
+
+
+setup(
+    name='cm',
+    version=__version__,
+    author=__author__,
+    author_email='me@mosquito.su',
+    license="MIT",
+    description="Commandline Magic",
+    platforms="all",
+    classifiers=[
+        'Environment :: Console',
+        'Programming Language :: Python',
+    ],
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+)
